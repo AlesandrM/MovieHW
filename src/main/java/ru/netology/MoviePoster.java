518b4f21b38cc3c8ddd8060b0e3442dc8b70edfa
Aleksandr Madzharov <madzharov-aleks@mail.ru>
2022-04-07 14:33:43 +0500

    second commit

--- src/main/java/ru/netology/MoviePoster.java
+++ src/main/java/ru/netology/MoviePoster.java
@@ -43,4 +43,6 @@ public class MoviePoster {
     }
 
 
+    public void add(Movie first) {
+    }
 }
--- src/test/java/ru/netology/MoviePosterTest.java
+++ src/test/java/ru/netology/MoviePosterTest.java
@@ -1,11 +1,15 @@
 package ru.netology;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoviePosterTest {
+
+    private MoviePoster manager = new MoviePoster();
+
 
     private final Movie first = new Movie(1, "http://", "Spider-Man: No Way Home", "Adventure");
     private final Movie second = new Movie(2, "http://", "No Time To Die", "Thriller");
@@ -19,70 +23,70 @@ class MoviePosterTest {
     private final Movie tenth = new Movie(10, "http://", "Cruella", "Crime");
 
 
-    @Test
+        @Test
         // должен показывать последние 10 фильмов
-    void shouldShowLast10Movies() {
-        MoviePoster poster = new MoviePoster(10);
-        poster.addMovie(first);
-        poster.addMovie(second);
-        poster.addMovie(third);
-        poster.addMovie(fourth);
-        poster.addMovie(fifth);
-        poster.addMovie(sixth);
-        poster.addMovie(seventh);
-        poster.addMovie(eighth);
-        poster.addMovie(ninth);
-        poster.addMovie(tenth);
-        Movie[] expected = {tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second, first};
-        Movie[] actual = poster.getLastAdd();
-        assertArrayEquals(expected, actual);
-    }
+        void shouldShowLast10Movies () {
+            MoviePoster poster = new MoviePoster(10);
+            poster.addMovie(first);
+            poster.addMovie(second);
+            poster.addMovie(third);
+            poster.addMovie(fourth);
+            poster.addMovie(fifth);
+            poster.addMovie(sixth);
+            poster.addMovie(seventh);
+            poster.addMovie(eighth);
+            poster.addMovie(ninth);
+            poster.addMovie(tenth);
+            Movie[] expected = {tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second, first};
+            Movie[] actual = poster.getLastAdd();
+            assertArrayEquals(expected, actual);
 
-    @Test
+        }
+
+        @Test
         // показывать 4 фильма из списка
 
-    void shouldShowCustomMovieLength() {
-        MoviePoster poster = new MoviePoster(4);
-        poster.addMovie(first);
-        poster.addMovie(second);
-        poster.addMovie(third);
-        poster.addMovie(fourth);
-        Movie[] expected = {fourth, third, second, first};
-        Movie[] actual = poster.getLastAdd();
-        assertArrayEquals(expected, actual);
-    }
+        void shouldShowCustomMovieLength () {
+            MoviePoster poster = new MoviePoster(4);
+            poster.addMovie(first);
+            poster.addMovie(second);
+            poster.addMovie(third);
+            poster.addMovie(fourth);
+            Movie[] expected = {fourth, third, second, first};
+            Movie[] actual = poster.getLastAdd();
+            assertArrayEquals(expected, actual);
+        }
 
 
-
-    @Test
+        @Test
         //должен показывать больше максимума
-    void mustShowOverMax() {
-        MoviePoster poster = new MoviePoster(15);
-        poster.addMovie(first);
-        poster.addMovie(second);
-        poster.addMovie(third);
-        poster.addMovie(fourth);
-        poster.addMovie(fifth);
-        poster.addMovie(sixth);
-        poster.addMovie(seventh);
-        poster.addMovie(eighth);
-        poster.addMovie(ninth);
-        poster.addMovie(tenth);
-        Movie[] expected = {tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second, first};
-        Movie[] actual = poster.getLastAdd();
-        assertArrayEquals(expected, actual);
+        void mustShowOverMax () {
+            MoviePoster poster = new MoviePoster(15);
+            poster.addMovie(first);
+            poster.addMovie(second);
+            poster.addMovie(third);
+            poster.addMovie(fourth);
+            poster.addMovie(fifth);
+            poster.addMovie(sixth);
+            poster.addMovie(seventh);
+            poster.addMovie(eighth);
+            poster.addMovie(ninth);
+            poster.addMovie(tenth);
+            Movie[] expected = {tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second, first};
+            Movie[] actual = poster.getLastAdd();
+            assertArrayEquals(expected, actual);
 
-    }
+        }
 
-    @Test
+        @Test
         // ни чего не показывать
-    void mustShowNothing() {
-        MoviePoster poster = new MoviePoster();
-        Movie[] actual = poster.getLastAdd();
-        Movie[] expected = {};
-        assertArrayEquals(expected, actual);
+        void mustShowNothing () {
+            MoviePoster poster = new MoviePoster();
+            Movie[] actual = poster.getLastAdd();
+            Movie[] expected = {};
+            assertArrayEquals(expected, actual);
+        }
+
+
     }
-
-
-}
 
